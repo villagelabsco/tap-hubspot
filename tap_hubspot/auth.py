efab3d3adcb28ca46c38cbab8b828aa1476d373a
Hadrian de Oliveira <hadrian@villagelabs.co>
2024-08-22 15:16:27 -0300

    feat: also use env for oauth credentials

--- tap_hubspot/auth.py
+++ tap_hubspot/auth.py
@@ -1,7 +1,7 @@
 from singer_sdk.authenticators import OAuthAuthenticator, SingletonMeta
 from singer_sdk.streams import RESTStream
 from typing import Any, Mapping, TypedDict, TypeGuard
-
+import os
 
 class HubSpotOAuthAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
     def __init__(self, stream: RESTStream) -> None:
@@ -12,11 +12,14 @@ class HubSpotOAuthAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
 
     @property
     def oauth_request_body(self) -> dict:
+        env_client_id = os.getenv("TAP_HUBSPOT_CLIENT_ID")
+        env_client_secret = os.getenv("TAP_HUBSPOT_CLIENT_SECRET")
+        env_refresh_token = os.getenv("TAP_HUBSPOT_REFRESH_TOKEN")
         return {
-            "client_id": self.config["client_id"],
-            "client_secret": self.config["client_secret"],
+            "client_id": self.config["client_id"] or env_client_id,
+            "client_secret": self.config["client_secret"] or env_client_secret,
             "grant_type": "refresh_token",
-            "refresh_token": self.config["refresh_token"],
+            "refresh_token": self.config["refresh_token"] or env_refresh_token,
         }
 
 
